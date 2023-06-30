520138e2af7e8892ef4e396242b455d7cb025173
Evgenii Vlasov <senzik@gmail.com>
2023-06-30 13:56:18 +0300

    firstTry

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,37 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.beans.Statement;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl usersDao = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
+        usersDao.createUsersTable();
 
     }
 
     public void dropUsersTable() {
-
+        usersDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        usersDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        usersDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+       return usersDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        usersDao.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
+import com.mysql.jdbc.Driver;
+import jm.task.core.jdbc.model.User;
+
+import java.sql.*;
+
+import java.sql.DriverManager.*;
+
+public class Util  {
     // реализуйте настройку соеденения с БД
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try  {
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
+
+
+
 }
+
+
